72cd6ded250125c49ccda714b48d105b552e1037
Sourya Roy <souryaroy30@gmail.com>
2025-12-30 16:34:55 +0530

    graceful shutdown

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/dangbros/student-api/internal/config"
 )
@@ -26,10 +32,30 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Printf("server started %s", cfg.HTTPServer.Address)
-	err := server.ListenAndServe()
+	slog.Info("server started", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start the server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatal("failed to start the server")
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
 
+	slog.Info("server shutdown successfully!1")
 }
